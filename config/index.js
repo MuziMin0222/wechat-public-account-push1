1e6d72871e8475d39136223755569f0b85c55b51
muzimin <40995489+MuziMin0222@users.noreply.github.com>
2022-09-08 17:13:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "wxb386d38191d1077b",
+  APP_ID: "wxb80a20cc3dec7e98",
 
   // 公众号APP_SECRET
-  APP_SECRET: "1c5df27a13c0ad6dd0140461fe7a1332",
+  APP_SECRET: "5c25049df2c1c0fd6859dc3537f0f691",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
@@ -29,22 +29,14 @@ export const config = {
       // 想要发送的人的名字
       name: "珍珍小宝贝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "gh_7f0eb4da87cd",
+      id: "oQdQa6w2B9FhAhKVpVOu4aFM6Y40",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "4m6mJtOAPt6Q9ZdxhznUyr8Kd0ShwGJtOdfptCkvGz4",
+      useTemplateId: "7WMRSlTSSdGhd7cHhv7NJysCqmDVgdIVoFowVYcJHiE",
       // 所在省份
       province: "广东",
       // 所在城市
       city: "广州",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "小李同志", 
-      id: "gh_7f0eb4da87cd",
-      useTemplateId: "4m6mJtOAPt6Q9ZdxhznUyr8Kd0ShwGJtOdfptCkvGz4",
-      province: "广东",
-      city: "广州",
       openUrl: "https://wangxinleo.cn"
     }, 
 
@@ -53,7 +45,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "7WMRSlTSSdGhd7cHhv7NJysCqmDVgdIVoFowVYcJHiE",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -61,7 +53,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "wx9c72800fd2dc6c4c",
+      id: "oQdQa6w2B9FhAhKVpVOu4aFM6Y40",
     }, 
   ],
     
